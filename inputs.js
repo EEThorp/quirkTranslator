7e574ccf81a6bd8be4c89e8fbc5e280a346b2b8c
Liz Thorp <liz.thorp@hey.com>
2025-07-11 15:02:23 +0100

    Split quirk translator fork into modules
    Remove the input, punctuation, and regex filter sections into their own modules.
    Add in export and import code to connect
    Add array declarations in each translator to fix new reference error of undeclared array.

---
+++ inputs.js
@@ -0,0 +1,31 @@
+//Values which can be modified from the website interface, as well as their default values if no option is selected. This will also contain the script for button activation etc.
+
+// text input. In future, userInput will be filled in from website form fill.
+let userInput = ""
+const defaultInput = "This is test text. The quick brown fox jumped over the lazy dog. Yes! There are commas, see? FEE. Numbers 0123456789. Terezi 413. E and H for Feferi. To two too into? Together. to too to. too. rito toddy To Too To. toon Into Today Tomorrow Together Tonight today into tomorrow tonight together intone Eight, gate, hate, great, fate, trait, faith. We have an elipsis as well... and words after. 0_0 and o_0 and 0u0 and O_O and :) and :D emotes. It's apostrophes too. KK KN WOW THIS";
+let input = userInput || defaultInput
+
+//This variable will be added to if the user selects a non-standard workskin formatting colour, otherwise it will be empty. The exact formatting script will be filled as the dropdown menu value, it will be the same as the options wiuthin the text colours module, but won't pull from there.
+let workskinCustom = ""
+
+/*Section for future flags that will be used to determine translation variations, these will be filled via tickbox on the website and used to turn on or off certain translation sections. Include description of what each flag will do. */
+//flag for whether to include the chat handle code in the output or not
+let handleOmit = false
+//flag for Jade's use of commas
+let jadeComma = false
+//flag for Jake's use of commas
+let jakeComma = false
+//flag for blind sollux text style. Is exclusive to solluxHalfDead
+let solluxBlind = false
+//flag for half-dead sollux text style. Is exclusive to solluxBlind.
+let solluxHalfDead = false
+//flag for nepeta and Equius' text to begin with their meowrail emote text. default is true.
+let meowrailsStart = true
+//flag for more extensive vriska 8-replacements as these increase when she's stressed or angry.
+let vriskaAngry = false
+//flag for emc2's hal's troll typing quirk colour formatting
+let halQuirk = false
+//flag for including workskin formatting
+let workskinCode = false
+
+export { userInput, defaultInput, input, workskinCustom, handleOmit, jadeComma, jakeComma, solluxBlind, solluxHalfDead, meowrailsStart, vriskaAngry, halQuirk, workskinCode };
---
+++ punctuation.js
@@ -0,0 +1,77 @@
+//punctuation rules and also functions stored here.
+
+import { startCapRegex, capsRegex } from './regexFilters.js';
+
+// punctuation to be omitted lists
+const punctuationAll = [",", ".", "!", "?", ":", ";", "'"]
+const davePunctuation = [",", "!", ":", ";", "'"]
+const jadePunctuationNoComma = [",", "'"]
+const jadePunctuationComma = ["'"]
+const aradiaPunctuation = [",", ".", "?", ";", "'"]
+const nepetaPunctuation = [",", ".", "'"]
+const tereziPunctuation = [".", ",", "'"]
+const cronusPunctuation = ["'"]
+const terminalPunctuation = [".", "!", "?"]
+const gamzeePunctuation = [" ", ",", ".", "!", "?", ":", ";", "'"]
+const psiiPunctuation = [",", ":", ";"]
+
+//identifies capital letters after punctuation and a space and returns an array of all matches as capsLocation array. Iterates through every match with while not null as its logic and pushes the index of all matches.
+const capsIdentifier = inputText => {
+    let capsLocationArray = []
+    let match
+    while ((match = startCapRegex.exec(inputText)) != null) {
+        capsLocationArray.push(match.index);
+    }
+    return capsLocationArray
+}
+
+//for capitalising first word character after punctuation, as identified with the capsIdentifier. Includes first letter of message as well.
+const capitalizeAtIndices = (text, indices) => {
+    // Convert string to array for easier manipulation
+    let textArray = text.split('')
+    indices.push("0")
+    
+    // Convert characters at specified indices to uppercase
+    indices.forEach(index => {
+        if (index < textArray.length) {
+            textArray[index] = textArray[index].toUpperCase()
+        }
+    })
+    
+    // Join back into string
+    return textArray.join('')
+}
+
+//for UNcapitalising first word character after punctuation, as identified with the capsIdentifier. Includes first letter of message as well.
+const unCapitalizeAtIndices = (text, indices) => {
+    // Convert string to array for easier manipulation
+    let textArray = text.split('')
+    indices.push("0")
+    // Convert characters at specified indices to lowercase
+    indices.forEach(index => {
+        if (index < textArray.length) {
+            textArray[index] = textArray[index].toLowerCase()
+        }
+    })
+    
+    // Join back into string
+    return textArray.join('')
+}
+
+//This is the caps chain checker, some characters use all lowercase unless they're shouting, so to allow for this the caps checker looks at the input to tell the difference between something capitalised which the quirk should turn into lowercase and shouting text that should remain in uppercase. It takes three values, char, prevChar, nextChar. char will be assigned as the present i character, prevChar is the character before the current, and nextChar is the one after. If there is no character present because it's the first or last in the input string then a null "" will be used in its place. The three are tested to see if they are capitals, and if so they add one to the capsCount, which is used later in the translators.
+
+const capsChain = (input, index) => {
+    const char = input[index];
+    const prevChar = input[index - 1] || "";
+    const nextChar = input[index + 1] || "";
+    
+    // Count capitals: current + adjacent
+    let capsCount = 0;
+    if (/[A-Z]/.test(prevChar)) capsCount++;
+    if (/[A-Z]/.test(char)) capsCount++;
+    if (/[A-Z]/.test(nextChar)) capsCount++;
+    
+    return capsCount;
+};
+
+export { punctuationAll, davePunctuation, jadePunctuationNoComma, jadePunctuationComma, aradiaPunctuation, nepetaPunctuation, tereziPunctuation, cronusPunctuation, terminalPunctuation, gamzeePunctuation, psiiPunctuation, capsIdentifier, capitalizeAtIndices, unCapitalizeAtIndices, capsChain };
